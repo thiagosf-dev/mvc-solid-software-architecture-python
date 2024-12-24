71c7be04fe0878e6e44195e84f26d369b50c9846
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-12-24 00:50:12 -0300

    config: Implemeting entities in models for sqlite

---
+++ src/models/sqlite/entities/people.py
@@ -0,0 +1,23 @@
+from sqlalchemy import BIGINT, Column, ForeignKey, String
+from ....models.sqlite.settings.base import Base
+
+
+class PeopleTable(Base):
+    __tablename__ = 'people'
+
+    id = Column(BIGINT, primary_key=True)
+
+    first_name = Column(String, nullable=False)
+
+    last_name = Column(String, nullable=False)
+
+    age = Column(BIGINT, nullable=False)
+
+    pets_id = Column(BIGINT, ForeignKey('pets.id'))
+
+    def __repr__(self):
+        return (
+            f"People [id={self.id}, first_name={
+                self.first_name}, last_name={self.last_name}, "
+            f"age={self.age}, pets_id={self.pets_id}]"
+        )
---
+++ src/models/sqlite/entities/pets.py
@@ -0,0 +1,15 @@
+from sqlalchemy import BIGINT, Column, String
+from ....models.sqlite.settings.base import Base
+
+
+class PetsTable(Base):
+    __tablename__ = 'pets'
+
+    id = Column(BIGINT, primary_key=True)
+
+    name = Column(String, nullable=False)
+
+    type = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"Pets [id={self.id}, name={self.name}, type={self.type}]"
