c39ffbc83b2c615567e8b0989f45991d62d99d49
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-12-24 00:30:26 -0300

    config: Configuring pytest and coverage and sqlalchemy and sqlite database

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -1,7 +1,9 @@
+import pytest
 from sqlalchemy import Engine
 from .connection import db_connection_handler
 
 
+@pytest.mark.skip(reason="Interation with database")
 def test_connect_to_db():
     enginer = db_connection_handler.get_engine()
     assert enginer is None
