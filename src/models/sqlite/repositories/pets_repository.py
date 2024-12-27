211c757f7e3ae6afccfff3840d39270473f29812
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2024-12-27 11:17:32 -0300

    feat: Implemeting pets_repository with first test

--- src/models/sqlite/repositories/pets_repository.py
+++ src/models/sqlite/repositories/pets_repository.py
@@ -14,3 +14,18 @@ class PetsRepository:
                 return pets
             except NoResultFound:
                 return []
+
+    def delete_pet(self, name: str) -> None:
+        with self.__db_connection as database:
+            try:
+                (
+                    database
+                    .session
+                    .query(PetsTable)
+                    .filter(PetsTable.name == name)
+                    .delete()
+                )
+                database.session.commit()
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,13 +1,19 @@
+# TESTES DE INTEGRAÇÃO
+
 import pytest
 from .pets_repository import PetsRepository
 from ....models.sqlite.settings.connection import db_connection_handler
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="Interation with the database")  # pragma: no cover
 def test_list_pets():
-    pets_repository = PetsRepository(db_connection_handler)
-    pets = pets_repository.list_pets()
-    print()
-    print(pets)
+    repo = PetsRepository(db_connection_handler)
+    repo.list_pets()
+
+
+@pytest.mark.skip(reason="Interation with the database")  # pragma: no cover
+def test_delete_pet():
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pet("belinha")
